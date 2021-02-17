867e18a91b2c1f3435656b9512645b24a473e91a
Kevin <kevin.malby@gmail.com>
2021-02-16 22:18:13 -0800

    Attempting to layout maker profile page

--- src/modules/maker-profile/components/MakerBanner.vue
+++ src/modules/maker-profile/components/MakerBanner.vue
@@ -1,3 +1,29 @@
 <template>
-    
+  <section>
+    <header>
+      <div class="text-h4">{{ makerName }}</div>
+    </header>
+    <div class="row justify-between">
+      <p class="col">{{ description }}</p>
+      <div class="col col-md-auto">
+        <q-list>
+          <q-item><q-btn>Follow</q-btn></q-item>
+          <q-item><q-btn>Contact</q-btn></q-item>
+          <q-item><q-btn>Patreon</q-btn></q-item>
+        </q-list>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  props: ["description", "makerName"],
+};
+</script>
+
+<style scoped>
+section {
+    border: 1px solid black;
+}
+</style>
--- src/modules/maker-profile/components/MakerProfile.vue
+++ src/modules/maker-profile/components/MakerProfile.vue
@@ -1,3 +1,61 @@
 <template>
-    <h5>This is some maker profile page</h5>
+  <maker-banner
+    :description="description"
+    makerName="Test Name"
+  ></maker-banner>
+  <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="mails" label="Mails" />
+          <q-tab name="alarms" label="Alarms" />
+          <q-tab name="movies" label="Movies" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+  
 </template>
+
+<script>
+import { ref } from "vue";
+import MakerBanner from "./MakerBanner.vue";
+
+export default {
+  components: {
+    MakerBanner,
+  },
+  setup() {
+    const description = ref(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    );
+
+    return {
+        description
+    };
+  },
+};
+</script>
