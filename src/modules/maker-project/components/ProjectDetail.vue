a4368d39790192d2996eff90f2a649d0c3d9dd95
Kevin <kevin.malby@gmail.com>
2021-03-06 09:40:41 -0800

    Project Detail - Added scroll area

--- src/modules/maker-project/components/ProjectDetail.vue
+++ src/modules/maker-project/components/ProjectDetail.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="video-container">
-    <q-video
-      v-if="hasVideoOverview"
-      src="https://www.youtube.com/embed/gyp9EGhM368"
-      :ratio="16 / 9"
-    ></q-video>
-  </div>
-  <q-editor v-model="projectDescription" min-height="5rem" />
+  <q-scroll-area class="project-content">
+    <div>
+      <q-video
+        v-if="hasVideoOverview"
+        src="https://www.youtube.com/embed/gyp9EGhM368"
+        :ratio="16 / 9"
+      ></q-video>
+    </div>
+    <div v-html="projectDescription"></div>
+  </q-scroll-area>
 </template>
 
 <script>
@@ -14,7 +16,27 @@ import { ref } from "vue";
 export default {
   setup() {
     const hasVideoOverview = ref(true);
-    const projectDescription = ref("Here is some editor text");
+    const projectDescription = ref(`
+    <h1>HTML Ipsum Presents</h1>
+
+<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+<h2>Header Level 2</h2>
+
+<ol>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ol>
+
+<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+
+<h3>Header Level 3</h3>
+
+<ul>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ul>
+    `);
 
     return {
       hasVideoOverview,
@@ -25,4 +47,7 @@ export default {
 </script>
 
 <style scoped>
+.project-content {
+  height: 75vh;
+}
 </style>
