2aef61fbc485e7093a69efdbd0dd2cf0749bc9d9
Kevin <kevin.malby@gmail.com>
2021-03-13 15:10:03 -0800

    Added layout for message content area and input control

--- src/modules/user-inbox/MessageContent.vue
+++ src/modules/user-inbox/MessageContent.vue
@@ -1,1 +1,161 @@
-<template>This is the message content</template>
+<template>
+  <div class="column message-content-container">
+    <q-toolbar class="bg-secondary text-white shadow-2">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+
+      <q-space />
+
+      <q-btn round dense flat icon="send" />
+    </q-toolbar>
+    <div class="col">
+      <q-scroll-area class="chat-area">
+        <div class="q-pa-md row justify-center">
+          <div>
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="You"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Christina"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[
+                'doing fine, how are you? What if we let this message get really long, will it expand to accommodate or just line break?',
+              ]"
+              stamp="4 minutes ago"
+            />
+          </div>
+        </div>
+      </q-scroll-area>
+    </div>
+    <div class="col-2">
+      <message-entry></message-entry>
+    </div>
+  </div>
+</template>
+
+<script>
+import MessageEntry from "./MessageEntry";
+export default {
+  components: {
+    MessageEntry,
+  },
+};
+</script>
+
+<style scoped>
+.message-content-container {
+  height: 100%;
+}
+.chat-area {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/modules/user-inbox/MessageEntry.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-sm row justify-center items-center message-input-container">
+    <q-input
+      class="message-input"
+      color="teal"
+      outlined
+      v-model="text"
+      autogrow
+      placeholder="Enter Message"
+      input-style="max-height: 6em"
+    >
+      <template v-slot:after>
+        <q-btn round dense flat icon="send" />
+      </template>
+    </q-input>
+  </div>
+</template>
+
+<style scoped>
+.message-input {
+  width: 50%;
+}
+
+.message-input-container {
+  height: 100%;
+}
+</style>
+
